a4c01749a969bdfc4d3597620534dea489e03425
pheyishayo <amzatsayi@yahoo.com>
2023-10-17 12:17:55 -0700

    task0

--- main.h
+++ main.h
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int printf_char(va_list *arg);
+int _strlen(char *d);
+int printf_string(va_list arg);
+int _strlenc(const char *d);
+int printf_37(void);
+
+#endif /* MAIN_H */
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
