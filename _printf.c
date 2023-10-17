cf400352c29bd097b7b74ac7942e642aea8e3ec5
mohamadm890 <mohamad890098@gmail.com>
2023-10-17 01:42:07 -0700

    d

--- _printf.c
+++ _printf.c
@@ -8,44 +8,24 @@
  */
 int _printf(const char *format, ...)
 {
+int printed = 0;
 va_list args;
-int len = 0;
 va_start(args, format);
-for (int p = 0; format[p] != '\0'; p++)
+while (*format != '\0')
 {
-if (format[p] == '%')
+if (*format == '%')
 {
-p++;
-if (format[p] == 'c')
-{
-char x = va_arg(args, int);
-write(1, &x, 1);
-len++;
-}
-else if (format[p] == 's')
-{
-const char *str = va_arg(args, const char *);
-for (int i = 0; str[i] != '\0'; i++)
-{
-write(1, &str[i], 1);
-len++;
-}
-}
-else if (format[p] == '%')
-{
-write(1, "%", 1);
-len++;
+format++;
+printed = choices(format, args, printed);
+format++;
 }
 else
 {
-}
-}
-else
-{
-write(1, &format[p], 1);
-len++;
+_putchar(*format);
+printed++;
+format++;
 }
 }
 va_end(args);
-return (len);
+return (printed);
 }
--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-int _putchar(char c)
+int _putchar(char c);
+int Choices(const char *format, va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_char(va_list args, int printed);
+int printf_integer(va_list args, int printed);
 #endif /* MAIN_H */
 
